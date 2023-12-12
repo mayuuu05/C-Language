26904ffc31769f968c4ae8fb795f96f4e7323649
Mayuri Purhoit <149376263+mayuuu05@users.noreply.github.com>
2023-12-12 11:53:30 +0530

    Update reverse array without using another array.c

--- Assortment/reverse array without using another array.c
+++ Assortment/reverse array without using another array.c
@@ -1,2 +1,31 @@
 //8. Wap to find the reverse array without using another array.
 
+#include<stdio.h>
+main()
+{
+	 int n;
+	printf("Enter the size of array :");
+	scanf("%d",&n);
+	int a[n];
+	int i;
+	for(i=0;i<n;i++)
+	{
+		printf("a[%d] : ",i);
+		scanf("%d",&a[i]);
+		
+	}
+	
+		for(i=0;i<n;i++)
+	{
+		
+		printf(" %d",a[i]);
+		
+	}
+	
+	   printf("\n reverse array is :");
+	   
+	for(i=n-1;i>=0;i--)
+	{
+		printf("  %d",a[i]);
+	}
+}
