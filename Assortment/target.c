d806eceed5f0fc90e2890472da954939cafb09c4
Mayuri Purhoit <149376263+mayuuu05@users.noreply.github.com>
2023-12-12 11:54:43 +0530

    Update target.c

--- Assortment/target.c
+++ Assortment/target.c
@@ -1,3 +1,33 @@
 //6. Write a program in C to find a pair of target value given by user.
 
 
+#include<stdio.h>
+main()
+{
+	int n;
+	printf("enter n :");
+	scanf("%d",&n);
+	int i,j;
+	int a[n];
+	
+	for(i=0; i<n; i++)
+	{
+		printf("enter a[%d]",i);
+		scanf("%d",&a[i]);
+	}
+	int x;
+	printf("enter value :");
+	scanf("%d",&x);
+	
+	for(i=0; i<n; i++)
+	{
+		for(j=i+1; j<n; j++)
+		{
+			if(a[i]+a[j]==x)
+			{
+				printf("%d,%d\n",a[i],a[j]);
+			}
+		
+		}
+	}
+}
